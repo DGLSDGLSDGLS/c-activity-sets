091c81111522063243a0171c04e591b93d75d115
unknown <dlavanya.bsc23@rvu.edu.in>
2024-01-29 12:26:37 +0530

    test

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,6 +1,6 @@
 // 1. Write a C program to print your name.
 #include<stdio.h>
 int main(){
-    printf("my name is Lavanya");
+    printf("my name is Lavanya \n ");
     return 0;
 }
--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,26 @@
-//2. Write a C program to add two numbers.
+#include<stdio.h>
+int input(){
+    int n;
+    printf("enter number to add: ");
+    scanf("%d",&n);
+    return n;
+}
 
+int sum(int a, int b, int s){
+    s=a+b;
+    return s;
+}
+
+void output(int s){
+    printf("the sum id %d",s);
+}
+
+int main(){
+    int a,b;
+    a=input();
+    b=input();
+    int s;
+    s=sum(a,b,s);
+    output(s);
+    return 0;
+}
