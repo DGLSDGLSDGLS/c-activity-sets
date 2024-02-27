f7782fe843c545d15c8e45628fdc167e36b3998e
D Lavanya <dlavanya.bsc23@rvu.edu.in>
2024-02-27 21:20:49 +0530

    test6

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,31 +1,53 @@
 //6. Write a program to find the index of a substring of a string
+//void input(char *a, char *b);
+//int sub(char *a, char *b);
+//void output(char *b, int index);
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-void input(char *a){
-    printf("enter a string to reverse: ");
+void input(char *a,char *b){
+    printf("enter a string: ");
     scanf("%s",a);
+    printf("enter substring: ");
+    scanf("%s",b);
 }
 
-int rev(char *a){
-    for(int i=0;a[i]!='\0';i++){
-        for(int x=i;a[i]=='\0';x--){
-            a[i]=a[x];
+int sub(char *a,char *b){
+    int string;
+    int subs;
+    string=strlen(a);  
+    subs=strlen(b);
+    int i;
+    int j;
+    for(i=0;i<=string-subs;i++){
+        for(j=0;j<subs;j++){
+            if(a[i+j]!=b[j]){
+                break;
+            }
+        }
+        if(j==subs){
+            return i;
         }
     }
-    return a;
+    return -1;
 }
 
-void output(char a){
-    pirntf("the reversed string is %s",a);
+void output(char *b, int index){
+    if(index!=-1){
+        printf("the substring index is %d",index);
+    }
+    else{
+        printf("no substring found");
+    }
 }
 
-int main()
-{
-    char a[100];
-    input(a);
-    int revv;
-    revv=rev(a);
-    output(revv);
+int main(){
+    char a[100],b[100];
+    input(a,b);
+    int substr;
+    substr=sub(a,b);
+    output(b,substr);
+    return 0;
 }
