e481fdd27216d7d4e4c624ec1facca603fba58c3
D Lavanya <dlavanya.bsc23@rvu.edu.in>
2024-03-05 14:52:57 +0530

    testsubstring

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,53 +1,35 @@
 //6. Write a program to find the index of a substring of a string
-//void input(char *a, char *b);
-//int sub(char *a, char *b);
-//void output(char *b, int index);
-
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
 
 void input(char *a,char *b){
-    printf("enter a string: ");
+    printf("enter big string: ");
     scanf("%s",a);
-    printf("enter substring: ");
+    printf("enter substring to find: ");
     scanf("%s",b);
 }
 
-int sub(char *a,char *b){
-    int string;
-    int subs;
-    string=strlen(a);  
-    subs=strlen(b);
+int sub(char *str,char *substr){
+    int len_a;
+    int len_b;
+    len_a=strlen(str);
+    len_b=strlen(substr);
     int i;
     int j;
-    for(i=0;i<=string-subs;i++){
-        for(j=0;j<subs;j++){
-            if(a[i+j]!=b[j]){
-                break;
+    for(i=0;len_a-len_b<len_a;i++){
+        for(j=0;j<len_b;j++){
+            if(substr[j]!=0){
+                printf("%d",j);
             }
         }
-        if(j==subs){
-            return i;
-        }
-    }
-    return -1;
-}
-
-void output(char *b, int index){
-    if(index!=-1){
-        printf("the substring index is %d",index);
-    }
-    else{
-        printf("no substring found");
     }
 }
 
 int main(){
-    char a[100],b[100];
+    char a[100];
+    char b[100];
     input(a,b);
-    int substr;
-    substr=sub(a,b);
-    output(b,substr);
+    int subb;
+    subb=sub(a,b);
     return 0;
 }
