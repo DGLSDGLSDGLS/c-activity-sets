547da79fe19dc67f7f90967ffaedcbba8119be00
D Lavanya <dlavanya.bsc23@rvu.edu.in>
2024-03-08 23:29:58 +0530

    test1

--- set04/problem01.c
+++ set04/problem01.c
@@ -17,6 +17,23 @@ void add(int num1,int den1,int num2,int den2,int *res_num,int *res_den){
     *res_den=(den1*den2);
 }
 
+int hcf(int res_num,int res_den){
+    int c;
+    while(res_den!=0){  //while is used 
+        c=res_num%res_den;
+        res_num=res_den;
+        res_den=c;
+    }
+    return res_num;
+}
+
+void simplify(int *res_num,int *res_den){
+    int d;
+    d=hcf(*res_num,*res_den);   // it is hcf , no need of any lops :>
+    *res_num=*res_num/d;
+    *res_den=*res_den/d;
+}
+
 void output(int res_num,int res_den){
     printf("the sum of two fractions is %d/%d",res_num,res_den);
 }
@@ -25,6 +42,7 @@ int main(){
     int num1,den1,num2,den2,res_num,res_den;
     input(&num1,&den1,&num2,&den2);
     add(num1,den1,num2,den2,&res_num,&res_den);
+    simplify(&res_num,&res_den);
     output(res_num,res_den);
     return 0;
 }
